9627ecce8ad60e8dceffd239dabbdad4c12fcebc
jaink-crypto <jaink987654321@gmail.com>
2025-09-22 17:53:10 +0530

    Update urls.py

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,7 +20,7 @@ from django.http import HttpResponse
 
 # Define the view directly here
 def home(request):
-    return HttpResponse("JENKINS IS SETUP AND CICD IS WORKING FINE NOW")
+    return HttpResponse("JENKINS IS SETUP AND CICD IS WORKING FINE NOW AGAIN")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
