a02ef3192bf0f6c18f3fe71f5bd1d31b4080aa28
jaink-crypto <jaink987654321@gmail.com>
2025-09-23 10:20:28 +0530

    Update urls.py

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,7 +20,7 @@ from django.http import HttpResponse
 
 # Define the view directly here
 def home(request):
-    return HttpResponse("JENKINS IS RUNNING FINE NOW.")
+    return HttpResponse("JENKINS IS RUNNING......")
 
 def check(request):
     return HttpResponse("CHECKING IS GOING ON FINE NOW.")
